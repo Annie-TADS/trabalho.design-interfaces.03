ade78052c16bc11f9c5570ad0a711776f64e0fcc
Annie-TADS <142127307+Annie-TADS@users.noreply.github.com>
2023-11-14 21:23:47 -0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,22 @@
 import gears from "../json/gears.json" assert {type: "json"};
 import characters from "../json/characters.json" assert {type: "json"};
 
-console.log(gears);
-console.log(characters);
+const data = gears.map((gear) => ({...gear, used_by: characters.filter((character) => gear.used_by.includes(character.name))}));
+console.log(data);
+
+const gearList = document.getElementById('gear_list');
+console.log(gearList);
+
+data.forEach((e) => {
+    const node = document.createElement("li");
+    node.textContent = e.name;
+
+    e.used_by.forEach((user) => {
+        const imagenode = document.createElement("img");
+        imagenode.src = user.icon;
+        imagenode.classList.add('list-img');
+        node.appendChild(imagenode);
+    })
+    
+    gearList.appendChild(node);
+})
